ee6f65494edaa95b9246edb4359dc3a22f71ec31
D.mon 24 <dchaminov@ya.ru>
2024-05-15 20:24:13 +0300

    test update

--- ss_test.m
+++ ss_test.m
@@ -1,28 +1,20 @@
 close all;
 clear all;                                  %#ok<CLALL>
 clc;
-ID=17                                       %#ok<NOPTS>
-[pss,sss]=SsGenerator.getSsSignalsByCellInfo(ID);
-figure
-stairs(pss)
-hold on
-stairs(sss)
-ylim([-1.5,1.5]);
-pss_test=nrPSS(ID);
-sss_test=nrSSS(ID);
-if pss_test~=pss
-    disp("PSS NOT ok")
-else
-    disp("PSS ok") 
+tests_passed=true;
+for ID=0:1:1007
+    [pss,sss]=SsGenerator.getSsSignalsByCellInfo(ID);
+    pss_test=nrPSS(ID);
+    sss_test=nrSSS(ID);
+    if pss_test~=pss
+        disp("PSS NOT ok with id=",ID)
+        tests_passed=false;
+    end
+    if sss_test~=sss
+        disp("SSS NOT ok with id=",ID) 
+        tests_passed=false;
+    end
 end
-if sss_test~=sss
-    disp("SSS NOT ok") 
-else
-    disp("SSS ok")
+if tests_passed
+    disp('The tests were passed successfully.')
 end
-% pfft=fft(pss);
-% sfft=fft(sss);
-
-wvtool(pss)
-wvtool(sss)
-
